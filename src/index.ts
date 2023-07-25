411beba8aaed9a7fbfc550a972308bf68f3374a7
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-25 11:50:57 +0100

    Added initial project implementation

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import express  from "express";
+
+const app = express() 
+
+
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.status(200).send( '/api/v0/' );
+  });
+
+  app.get( '/users', async ( req, res ) => {
+    const users = [{
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+          "street": "Kulas Light",
+          "suite": "Apt. 556",
+          "city": "Gwenborough",
+          "zipcode": "92998-3874",
+          "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+          }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+          "name": "Romaguera-Crona",
+          "catchPhrase": "Multi-layered client-server neural-net",
+          "bs": "harness real-time e-markets"
+        }
+      },
+      {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+          "street": "Victor Plains",
+          "suite": "Suite 879",
+          "city": "Wisokyburgh",
+          "zipcode": "90566-7771",
+          "geo": {
+            "lat": "-43.9509",
+            "lng": "-34.4618"
+          }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+          "name": "Deckow-Crist",
+          "catchPhrase": "Proactive didactic contingency",
+          "bs": "synergize scalable supply-chains"
+        }
+      },]
+    return res.status(200).json(users);
+  });
+
+app.listen(5000, () => {
+    console.log( `server running on 5000` );
+    console.log( `press CTRL+C to stop server` );
+})
